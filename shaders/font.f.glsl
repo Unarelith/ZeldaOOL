e024743c6fab41038d7169bb3872ba58d26c78ad
Quentin Bazin <quent42340@gmail.com>
2014-10-01 14:25:21 +0200

    [Font] Test 3.

--- shaders/font.f.glsl
+++ shaders/font.f.glsl
@@ -1,17 +1,18 @@
 #version 120
 
 varying vec2 f_texCoord;
-varying vec3 f_color;
+//varying vec3 f_color;
 
 uniform sampler2D u_tex;
 
 void main(void) {
-	vec4 finalColor = texture2D(u_tex, f_texCoord);
+	/*vec4 finalColor = texture2D(u_tex, f_texCoord);
 	
 	if(finalColor == vec4(0.0, 0.0, 0.0, 1.0)) {
 		finalColor = vec4(f_color, 1.0);
 	}
 	
-	gl_FragColor = finalColor;
+	gl_FragColor = finalColor;*/
+	gl_FragColor = texture2D(u_tex, f_texCoord);
 }
 
