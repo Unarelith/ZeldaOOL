4e45c242b82f066d7171d2dde40175852db57e7c
Quentin Bazin <quent42340@gmail.com>
2015-02-07 02:56:51 +0100

    [ZeldaOOL] Repo initialized.

--- shaders/game.f.glsl
+++ shaders/game.f.glsl
@@ -5,9 +5,6 @@ varying vec4 v_colorMod;
 varying vec4 v_color;
 
 uniform sampler2D u_tex;
-uniform int u_paletteID;
-
-vec4 colorInPalette(int paletteID, vec4 color);
 
 void main() {
 	vec4 textureColor = texture2D(u_tex, v_texCoord);
@@ -20,39 +17,6 @@ void main() {
 		finalColor = v_color;
 	}
 	
-	if(u_paletteID > 0) {
-		finalColor = colorInPalette(u_paletteID, finalColor);
-	}
-	
 	gl_FragColor = finalColor;
-}
-
-vec4 Color(int r, int g, int b) {
-	return vec4(r / 255.0, g / 255.0, b / 255.0, 1.0);
-}
-
-vec4 palettes[2 * 4] = vec4[](
-	vec4(1.0, 1.0, 1.0, 0.0),
-		Color(248, 208, 136),
-		Color(16, 168, 64),
-		Color(0, 0, 0),
-	vec4(1.0, 1.0, 1.0, 0.0),
-		Color(0, 0, 0),
-		Color(216, 0, 0),
-		Color(248, 176, 48)
-);
-
-int colorIDinPalette(int paletteID, vec4 color) {
-	for(int i = paletteID * 4 ; i < paletteID * 4 + 4 ; i++) {
-		if(floor(palettes[i] + 0.5) == floor(color + 0.5)) {
-			return i;
-		}
-	}
-	
-	return 0;
-}
-
-vec4 colorInPalette(int paletteID, vec4 color) {
-	return palettes[paletteID * 4 + colorIDinPalette(0, color)];
 }
 
