ee317b4827c69260c6bd9ef90c4f62a0440c16d6
Quentin Bazin <quent42340@gmail.com>
2019-02-26 16:42:36 +0100

    Migrated to C++14.

--- include/core/Config.hpp
+++ include/core/Config.hpp
@@ -17,8 +17,25 @@
 namespace {
 	constexpr const char *APP_NAME = "The Legend of Zelda";
 
-	constexpr float SCREEN_WIDTH  = 160;
-	constexpr float SCREEN_HEIGHT = 144;
+	constexpr int SCREEN_WIDTH  = 160;
+	constexpr int SCREEN_HEIGHT = 144;
+}
+
+#include <cassert>
+
+// FIXME: Pre-C++17 fix
+namespace std {
+	template<class T>
+	constexpr const T &clamp(const T& v, const T& lo, const T& hi)
+	{
+		return clamp(v, lo, hi, std::less<>());
+	}
+
+	template<class T, class Compare>
+	constexpr const T &clamp(const T &v, const T &lo, const T &hi, Compare comp)
+	{
+		return assert(!comp(hi, lo)), comp(v, lo) ? lo : comp(hi, v) ? hi : v;
+	}
 }
 
 #endif // CONFIG_HPP_
